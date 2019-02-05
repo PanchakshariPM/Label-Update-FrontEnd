fe0503240ea69c648b21c7738e6492bb9d6f21a0
PanchakshariPM <panchakshari.s@datafoundry.ai>
2019-02-05 17:52:56 +0530

    2nd updated app

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, NgModule, EventEmitter } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormBuilder, FormGroup, } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,8 +7,9 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  countryModel: any;
   projectNameModel: any;
+
+  createForm: FormGroup;
 
   states: string[] = [
     'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware',
@@ -20,49 +21,8 @@ export class CreateComponent implements OnInit {
     'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
   ];
 
-  projectName = new FormControl('', [Validators.required]);
-  docType = new FormControl('', [Validators.required]);
-  countryType = new FormControl('', [Validators.required]);
-
-  getErrorMessage() {
-    return this.projectName.hasError('required') ? 'You must enter a value' :
-      this.projectName.hasError('projectName') ? 'Not a valid name' :
-        '';
-  }
-
-  getErrorMessageForDocType() {
-    return this.docType.hasError('required') ? 'You must select a document' :
-      this.docType.hasError('docType') ? 'Not a valid document' :
-        '';
-  }
-
-  getErrorMessageForCountryType() {
-    return this.countryType.hasError('required') ? 'You must select a Country' :
-      this.countryType.hasError('countryType') ? 'Not a valid option' :
-        '';
-  }
-
-
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
-
-  afuConfig = {
-    multiple: true,
-    formatsAllowed: ".jpg,.png",
-    maxSize: "1",
-    uploadAPI: {
-      url: "https://example-file-upload-api",
-      // headers: {
-      // "Content-Type" : "text/plain;charset=UTF-8",
-      // "Authorization" : `Bearer ${token}`
-      // }
-    },
-    theme: "dragNDrop",
-    hideProgressBar: false,
-    hideResetBtn: false,
-    hideSelectBtn: false
-  };
-
 }
